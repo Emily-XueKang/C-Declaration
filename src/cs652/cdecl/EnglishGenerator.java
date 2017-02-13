08d568ba2e821593ed38eecb50a5044e2411e371
Emily-Xue Kang <xkang3@dons.usfca.edu>
2017-02-13 10:52:22 -0800

    generate visitor

--- src/cs652/cdecl/EnglishGenerator.java
+++ src/cs652/cdecl/EnglishGenerator.java
@@ -1,4 +1,5 @@
-package cs652.cdecl;
+//package cs652.cdecl;
+import org.antlr.v4.runtime.tree.ParseTree;
 
 public class EnglishGenerator extends CDeclBaseVisitor<String> {
 	// YOU MUST FILL THIS IN
--- src/cs652/cdecl/Tool.java
+++ src/cs652/cdecl/Tool.java
@@ -1,4 +1,4 @@
-package cs652.cdecl;
+//package cs652.cdecl;
 
 import org.antlr.v4.runtime.ANTLRFileStream;
 import org.antlr.v4.runtime.ANTLRInputStream;
@@ -7,6 +7,22 @@ import org.antlr.v4.runtime.tree.ParseTree;
 
 public class Tool {
 	public static void main(String[] args) {
+		String read = "sentence";
+		ANTLRInputStream input = new ANTLRInputStream(read);
+
+		// create a lexer that feeds off of input CharStream
+		CDeclLexer lexer = new CDeclLexer(input);
+		// create a buffer of tokens pulled from the lexer
+		CommonTokenStream tokens = new CommonTokenStream(lexer);
+		// create a parser that feeds off the tokens buffer
+		CDeclParser parser = new CDeclParser(tokens);
+
+		CDeclParser.EContext tree = parser.e();
+		//System.out.println(tree.toStringTree(parser));
+
+		EnglishGenerator visitor = new EnglishGenerator();
+		int v = visitor.visit(tree);
+		System.out.println(v);
 		// YOU MUST FILL THIS IN
 	}
 
